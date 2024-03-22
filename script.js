791a02e91cbf729daa1722d4e7f96a3023202f59
Reese Pollard <rpollard@gmail.com>
2024-03-21 23:35:09 -0700

    Base JS Calculator

---
+++ script.js
@@ -0,0 +1,181 @@
+let display = document.querySelector("#display");
+display.textContent = "69 x 420";
+
+const op = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => a / b,
+};
+
+display.textContent = "";
+
+// we need to put event listeners on each button i guess
+const buttonIds = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "zero",
+  "doublezero",
+  "dec",
+  "add",
+  "subtract",
+  "divide",
+  "multiply",
+  "clear",
+  "equals",
+];
+
+const newState = () => {
+  return {
+    currentVal: "",
+    storedVal: "",
+    op: "",
+    currentDecimal: true,
+    storedDecimal: true,
+    lockStoredVal: false,
+    lockCurrentVal: false,
+  };
+};
+
+let inputState = newState();
+
+const isNumber = (val) => {
+  if (isNaN(val)) {
+    return false;
+  }
+  return true;
+};
+
+const isOperator = (val) => {
+  if (val in op) {
+    return true;
+  }
+  return false;
+};
+
+const handleNumber = (val) => {
+  // if an operator is not defined we are editing storedVal
+  if (inputState.op.length === 0 && !inputState.lockStoredVal) {
+    // unlock decimal after something has been entered
+    if (inputState.storedVal.toString().length === 0) {
+      inputState.storedDecimal = false;
+    }
+    inputState.storedVal += val;
+    updateDisplayText(inputState.storedVal);
+  } else {
+    // if an operator is defined, we are editing currentVal
+    // currentVal gets locked when hit equals, this allows repeated tallying by tapping equals
+    if (inputState.lockCurrentVal) {
+      inputState.currentVal = val;
+      inputState.currentDecimal = false;
+      inputState.lockCurrentVal = false;
+    } else {
+      // unlock decimal once something is here
+      if (inputState.currentVal.toString().length === 0) {
+        inputState.currentDecimal = false;
+      }
+      inputState.currentVal += val;
+    }
+    updateDisplayText(inputState.currentVal);
+  }
+};
+
+const handleOperator = (val) => {
+  if (inputState.storedVal.toString().length > 0) {
+    if (inputState.op.length > 0 && !inputState.lockStoredVal) {
+      inputState.storedVal = op[inputState.op](
+        +inputState.storedVal,
+        +inputState.currentVal,
+      );
+    }
+    inputState.lockStoredVal = false;
+    inputState.currentVal = "";
+    inputState.op = val;
+  }
+  updateDisplayText(inputState.op);
+};
+
+const handleEquals = () => {
+  if (inputState.op.length === 0) {
+    return;
+  }
+  // if currentVal isn't defined when we calc, we set it to storedVal
+  if (inputState.currentVal.toString().length === 0) {
+    inputState.currentVal = inputState.storedVal;
+  }
+
+  inputState.storedVal = op[inputState.op](
+    +inputState.storedVal,
+    +inputState.currentVal,
+  );
+
+  inputState.currentDecimal = true;
+  inputState.storedDecimal = true;
+  inputState.lockStoredVal = true;
+  updateDisplayText(inputState.storedVal);
+};
+
+const handleDecimal = () => {
+  if (!isOperator(display.textContent)) {
+    if (
+      inputState.currentVal.toString().length > 0 &&
+      !inputState.lockCurrentVal &&
+      !inputState.currentDecimal
+    ) {
+      inputState.currentVal += ".";
+      inputState.currentDecimal = true;
+      updateDisplayText(inputState.currentVal);
+    } else if (
+      inputState.currentVal.toString().length === 0 &&
+      !inputState.lockStoredVal &&
+      !inputState.storedDecimal
+    ) {
+      inputState.storedVal += ".";
+      inputState.storedDecimal = true;
+      updateDisplayText(inputState.storedVal);
+    }
+  }
+  console.log("decimcal");
+};
+
+// main state machine that switches behavior depending on input type
+const handleInput = (e) => {
+  const val = e.target.innerText;
+
+  if (isNumber(val)) {
+    handleNumber(val);
+  } else if (isOperator(val)) {
+    handleOperator(val);
+  } else {
+    switch (val) {
+      case "C":
+        inputState = newState();
+        updateDisplayText(inputState.storedVal);
+        break;
+      case "=":
+        handleEquals();
+        break;
+      case ".":
+        handleDecimal();
+        break;
+    }
+  }
+
+  console.log(val);
+};
+
+buttonIds.map((b) => {
+  let butt = document.querySelector(`#${b}`);
+  butt.addEventListener("click", (e) => handleInput(e));
+});
+
+const updateDisplayText = (text) => {
+  display.textContent = text;
+};
